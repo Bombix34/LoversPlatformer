3cd815095e906256f35b1d2667c6654fcd41989c
Hugo <bombix34@gmail.com>
2020-01-15 22:46:24 +0100

    [ADD] debut hero overworms

---
+++ SamuraiCutter/Assets/Scripts/ScriptableObjects/HeroSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "OVERWORMS/Réglages hero")]
+public class HeroSettings : ScriptableObject
+{
+    [Header("Reglages Mouvements")]
+    [Range(1f, 50f)]
+    public float moveSpeed;
+
+    [Header("Réglages Saut")]
+    // [Range(1f, 50f)]
+    public float jumpForce = 500f;
+    public bool AirControl = false;
+    [Range(0f, 1f)]
+    public float AirControlMultiplicator = 0.5f;
+    [Range(0f, 1f)]
+    public float jumpInputTime = 0.35f;
+    public bool JumpNuance = false;
+}
---
+++ SamuraiCutter/Assets/Scripts/State.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State 
+{
+    protected string m_stateName;
+    protected ObjectManager m_objectManager;
+
+    public abstract void Enter();
+    public abstract void Execute();
+    public abstract void Exit();
+
+    //public abstract void OnReceiveMessage(Telegram message);
+
+}
